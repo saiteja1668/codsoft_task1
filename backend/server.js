712bb5d1e5da824defc4f8744101d7e615b4d6f6
A SAI TEJA <saitejachary129@gmail.com>
2026-09-23 18:58:44 +0530

    Add file listing route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const helmet = require("helmet");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+  accesskeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+
 app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
   }));
